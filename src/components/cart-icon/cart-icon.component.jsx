15c630da78314f55111298c17dcd3e7ecfe2a615
HavrylivDenys <denys.havryliv@gmail.com>
2020-07-07 19:22:08 +0300

    Added cart-item component for guds. Added counter for goods.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { toggleCartHidden } from "../../redux/cart/cart.actions.js"
+import { toggleCartHidden } from "../../redux/cart/cart.actions.js";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({ itemCount, toggleCartHidden }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
-const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumalatedQuantyty, cartItem) => accumalatedQuantyty + cartItem.quantity,
+    0
+  ),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
